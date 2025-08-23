38c24bd144106364e68acc12ac5ddbf812965485
Christopher Martínez <martinezacosta1998@gmail.com>
2025-08-23 15:12:18 -0500

    PayPal sandbox client id real

--- frontend/config.js
+++ frontend/config.js
@@ -2,6 +2,6 @@
 window.APP_CONFIG = {
   CURRENCY: "USD",
   PRICE_PER_PIXEL: 0.01,          // ajusta
-  PAYPAL_CLIENT_ID: "SB_CLIENT_ID", // sandbox primero
+  PAYPAL_CLIENT_ID: "AZlklM2eaRicIi_3btmm54Zya9xhTjrNqM4ggl3CNSGcSM2qg5l5ABllhGBd0j_SREg4UGhHymhXREyu", // sandbox primero
   ENDPOINT: "https://script.google.com/macros/s/TU_ID/exec" // tu Apps Script
 };
