9e02069f188dc72d3d72ec336caf167c31b8c015
Christopher Martínez <martinezacosta1998@gmail.com>
2025-08-23 16:57:21 -0500

    Precio por pixel > 0

--- frontend/config.js
+++ frontend/config.js
@@ -3,5 +3,5 @@ window.APP_CONFIG = {
   CURRENCY: "USD",
   PRICE_PER_PIXEL: 0.001,          // ajusta
   PAYPAL_CLIENT_ID: "AZlklM2eaRicIi_3btmm54Zya9xhTjrNqM4ggl3CNSGcSM2qg5l5ABllhGBd0j_SREg4UGhHymhXREyu", // sandbox primero
-  ENDPOINT: "https://script.google.com/macros/s/TU_ID/exec" // tu Apps Script
+  ENDPOINT: "https://script.google.com/macros/s/AKfycby6XbtzygRO7F18NBkuyddy7qMjZm1ZyJagDQSgioduS7qh1DInvR43mLyeRvlOHZBOZg/exec" // tu Apps Script
 };
