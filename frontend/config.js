3e8a271483c7f8ba75b52b648fcd6032cfb286fb
Christopher Martínez <martinezacosta1998@gmail.com>
2025-09-01 16:39:47 -0500

    Deploy: actualizar frontend (index/checkout/config + persistencia)

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 // frontend/config.js
 window.APP_CONFIG = {
   CURRENCY: "USD",
-  PRICE_PER_PIXEL: 0.001,          // ajusta
+  PRICE_PER_PIXEL: 0.0001,          // ajusta
   PAYPAL_CLIENT_ID: "AZlklM2eaRicIi_3btmm54Zya9xhTjrNqM4ggl3CNSGcSM2qg5l5ABllhGBd0j_SREg4UGhHymhXREyu", // sandbox primero
   ENDPOINT: "https://script.google.com/macros/s/AKfycbwsZVDCXZoc_n_FHNUXOt_w9KStFEiu0Z8IYkz60veBT2xKb59AhDU2G96w74dyN3Wm0w/exec" // tu Apps Script
 };
