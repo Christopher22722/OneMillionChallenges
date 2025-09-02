fc4ccdb4604d61454aab82546eeadf19c3abdcaa
Christopher22722 <martinezacosta1998@gmail.com>
2025-09-01 19:12:26 -0500

    Add files via upload

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+// frontend/config.js
+window.APP_CONFIG = {
+  CURRENCY: "USD",
+  PRICE_PER_PIXEL: 0.0001,          // ajusta
+  PAYPAL_CLIENT_ID: "AZlklM2eaRicIi_3btmm54Zya9xhTjrNqM4ggl3CNSGcSM2qg5l5ABllhGBd0j_SREg4UGhHymhXREyu", // sandbox primero
+  ENDPOINT: "https://script.google.com/macros/s/AKfycbwj4Lfly-_ni8y_kGDfaa94QwpSncMqzhnXXo4U1sf2yZ8JsWOQSWoaT6ZqBwkOLM3_4Q/exec" // tu Apps Script
+};
