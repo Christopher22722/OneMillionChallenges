913a95e08cb0b7bbe10960a0b3202353ea86ee65
Christopher22722 <martinezacosta1998@gmail.com>
2025-09-02 09:31:13 -0500

    Delete frontend directory

--- frontend/config.js
+++
@@ -1,8 +0,0 @@
-// frontend/config.js
-window.APP_CONFIG = {
-  CURRENCY: "USD",
-  PRICE_PER_PIXEL: 0.0001,          // ajusta
-  PAYPAL_CLIENT_ID: "AZlklM2eaRicIi_3btmm54Zya9xhTjrNqM4ggl3CNSGcSM2qg5l5ABllhGBd0j_SREg4UGhHymhXREyu", // sandbox primero
-  ENDPOINT: "https://script.google.com/macros/s/AKfycbwHdz7fJOoqjrrzGwl56elSoQphe8aHoip9MfC5-jntbzwAtLazblRWbuGDMjddm1LtJQ/exec" // tu Apps Script
-};
-
