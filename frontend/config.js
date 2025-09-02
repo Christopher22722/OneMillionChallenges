4a6494ffcc4e31b0db515bdc496774acb768e731
Christopher22722 <martinezacosta1998@gmail.com>
2025-09-01 21:21:11 -0500

    config.js

--- frontend/config.js
+++ frontend/config.js
@@ -5,3 +5,4 @@ window.APP_CONFIG = {
   PAYPAL_CLIENT_ID: "AZlklM2eaRicIi_3btmm54Zya9xhTjrNqM4ggl3CNSGcSM2qg5l5ABllhGBd0j_SREg4UGhHymhXREyu", // sandbox primero
   ENDPOINT: "https://script.google.com/macros/s/AKfycbwHdz7fJOoqjrrzGwl56elSoQphe8aHoip9MfC5-jntbzwAtLazblRWbuGDMjddm1LtJQ/exec" // tu Apps Script
 };
+
