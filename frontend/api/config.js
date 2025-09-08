402c08b74dfa5e1d43ada8d787ffa07456ea8b92
Christopher22722 <martinezacosta1998@gmail.com>
2025-09-07 21:01:03 -0500

    Delete frontend/api/config.js

--- frontend/api/config.js
+++
@@ -1,7 +0,0 @@
-// frontend/config.js
-window.APP_CONFIG = {
-  CURRENCY: "USD",
-  PRICE_PER_PIXEL: 0.0001,          // ajusta
-  PAYPAL_CLIENT_ID: "AZlklM2eaRicIi_3btmm54Zya9xhTjrNqM4ggl3CNSGcSM2qg5l5ABllhGBd0j_SREg4UGhHymhXREyu", // sandbox primero
-  ENDPOINT: "/api/grid"
-};
